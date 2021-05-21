440a1d3bc0f647eee9a94de8bdcac3b815a5bf5d
erafn <nathan.lerue@gmail.com>
2021-05-21 19:17:59 -0300

    Get started on the home page, and the CSS.

--- public/static/js/main.home.js
+++ public/static/js/main.home.js
@@ -4,16 +4,4 @@
         var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
         return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
       }
-    const zoomElement = document.querySelector(".zoom");
-    let zoom = 1;
-    const ZOOM_SPEED = 0.1;
-
-    document.addEventListener("wheel", function (e) {
-        if(!checkVisible(zoomElement)) return;
-        if (e.deltaY > 0) {
-            zoomElement.style.transform = `scale(${(zoom += ZOOM_SPEED)})`; 
-        } else {
-            zoomElement.style.transform = `scale(${(zoom -= ZOOM_SPEED)})`;
-        }
-    });
 }(this)
