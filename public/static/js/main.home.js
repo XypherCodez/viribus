6afb1a35eea7b43906c8bc2beb1b898cdac62d73
erafn <nathan.lerue@gmail.com>
2021-05-21 18:23:54 -0300

    --- Auto-Git Commit ---

--- public/static/js/main.home.js
+++ public/static/js/main.home.js
@@ -1,5 +1,8 @@
 !function (a){
     function a() {
-        
+        window.onscroll = function (d) {
+            var scroll = $(window).scrollToTop()
+            document.getElementById('weight-container').style.width(100 + scroll/5)
+        }
     }
 }(this)
