d443eb9c4afb3945ab83e7323131c280f97ded43
erafn <nathan.lerue@gmail.com>
2021-05-25 19:04:19 -0300

    Get started on the Python based WebSocket API

--- src/API/source/ws/source/start.py
+++ src/API/source/ws/source/start.py
@@ -1,2 +1,17 @@
 def start(): 
-    print("Starting WebSocket connection..")
+    print("[PYTHON]: Starting WebSocket connection..")
+    import tornado.ioloop
+    import tornado.web
+    class MainHandler(tornado.web.RequestHandler):
+        def get(self):
+            self.write("Hello, world!");
+    
+    def make_app():
+        return tornado.web.Application([
+            (r"/", MainHandler),
+        ])
+    
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start();
+    print("[WEBSOCKET]: Started websocket server.")
