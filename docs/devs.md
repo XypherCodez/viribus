8316fab038be5641002aa20c99a219f8d500a514
Era <erafn.yt@gmail.com>
2021-06-09 17:26:02 -0300

    --- Auto-Git Commit ---

---
+++ docs/devs.md
@@ -0,0 +1,34 @@
+#App
+
+All code for native mobile apps, IOS & Android
+
+#Docs
+
+The documentation you're currently reading.
+
+#Private
+
+Private API functions.
+
+#Public
+
+Public API functions.
+
+#Server
+
+Code for the backend server communication.
+
+#Src
+
+Source code for the main webpage, etc.
+
+#API
+
+API code.
+
+#Config
+
+Config Files etc.
+
+#React
+
--- src/react/src/components/Home.jsx
+++ src/react/src/components/Home.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 function Home() {
     return (
         <div className="header">
-            <h1>Hi</h1>
+            <h1><span className="header-top-text-large">Viribus</span><span className="header-top-text-small">Your fitness app</span></h1>
+            <div className="header-top-elements"id="header-top">
+                <div className="header-top-elements-left">
+                    <h3><a href="/about">About</a></h3>
+                    <h3><a href="https://github.com/viribusapp/viribus">Github</a></h3>
+                    <h3><a href="/fordevs">For Developers</a></h3>
+                    <h3><a href="/contact">Contact Us</a></h3>
+                </div>
+                <div className="header-top-elements-right">
+                    <h3>hi</h3>
+                </div>
+            </div>
         </div>
     )
 }
