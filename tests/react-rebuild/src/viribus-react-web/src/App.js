a25077e00e44ee715b6cdb3540ac166d844b54a1
Era <erafn.yt@gmail.com>
2021-06-08 21:45:48 -0300

    Get started on the new react-based website

---
+++ tests/react-rebuild/src/viribus-react-web/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contract';
+
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <div>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/about' component={About} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ tests/react-rebuild/src/viribus-react-web/src/components/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div>
+            <h1>HI FUCKING WORLD</h1>
+        </div>
+    )
+}
+export default About
